5a3a1faae7c8f1d0e46b30701745566a9c0fa091
Dosheng Wang <1005339793@qq.com>
2026-03-17 20:18:31 +0800

    Update rq.py

--- V2/SID/CRQVAE/rq.py
+++ V2/SID/CRQVAE/rq.py
@@ -1,11 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# from .cvq import CosineVectorQuantizer
-from .cvq_ema import CosineVectorQuantizer
-
-# from .vq import CosineVectorQuantizer
-
+from .cvq import CosineVectorQuantizer
 
 class ResidualVectorQuantizer(nn.Module):
 
